c5f66883b83c81d09f899c24285928f12155d739
Ludmila Razumovskaya <LRazumovskaya@abbyy.ru>
2017-12-14 21:09:08 +0300

    bug fix

--- js/pictures.js
+++ js/pictures.js
@@ -1,27 +1,32 @@
 'use strict';
 
-var PHOTOS_COUNT = 25;
+var PHOTOS_COUNT = 24;
 var PHOTOS_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var minLikes = 15;
-var maxLikes = 200;
-var minComments = 0;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+var MIN_COMMENTS = 0;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var pictureTemplate = document.querySelector('#picture-template').content;
-document.querySelector('.gallery-overlay').classList.remove('hidden');
 var galleryOverlayPreview = document.querySelector('.gallery-overlay-preview');
-
+var pictures_c = document.querySelector('.pictures');
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
 var generatePictures = function () {
   var pictures = [];
-  for (var i = 1; i <= PHOTOS_COUNT; i++) {
+    
+  for (var i = 0; i <= PHOTOS_COUNT; i++) {
+      
+     var number= i + 1;
     pictures[i] =
     {
-      url: 'photos/' + i + '.jpg',
-      likes: getRandomNumber(minLikes, maxLikes),
-      comments: getRandomNumber(minComments, PHOTOS_COMMENTS.length)
+      url: 'photos/' + number+ '.jpg',
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+      comments: getRandomNumber(MIN_COMMENTS, PHOTOS_COMMENTS.length)
     };
+  
   }
   return pictures;
 };
@@ -37,10 +42,10 @@ var renderPicture = function (photo) {
 
 var renderAllPictures = function () {
   var fragment = document.createDocumentFragment();
-  for (var i = 1; i < pictures.length; i++) {
+  for (var i = 0; i < PHOTOS_COUNT; i++) {
     fragment.appendChild(renderPicture(pictures[i]));
   }
-  galleryOverlayPreview.appendChild(fragment);
+  pictures_c.appendChild(fragment);
 };
 renderAllPictures();
 
@@ -49,4 +54,63 @@ var renderFirstPicture = function (photo) {
   galleryOverlayPreview.querySelector('.likes-count').textContent = photo.likes;
   galleryOverlayPreview.querySelector('.comments-count').textContent = photo.comments;
 };
-renderFirstPicture(pictures[1]);
+
+
+var picture = document.querySelector('.picture');
+var galleryOpen = document.querySelector('.gallery-overlay');
+var galleryClose = document.querySelector('.gallery-overlay-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+    
+};
+var openPopup = function () {
+  document.addEventListener('keydown', onPopupEscPress);
+    document.querySelector('.gallery-overlay').classList.remove('hidden');
+};
+var closePopup = function () {
+    document.removeEventListener('keydown', onPopupEscPress);
+document.querySelector('.gallery-overlay').classList.add('hidden');
+    
+};
+
+picture.addEventListener('click', function () {
+  openPopup();
+});
+
+picture.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+galleryClose.addEventListener('click', function () {
+  closePopup();
+});
+
+galleryClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+
+var photosElement = document.querySelectorAll('.picture');
+
+
+ 
+photosElement.forEach(function (item, index) {
+    
+    
+  var onItemClick = function (evt) {
+      evt.preventDefault();
+      openPopup();
+    renderFirstPicture(pictures[index]);
+  };
+
+item.addEventListener('click', onItemClick);
+
+});
+
